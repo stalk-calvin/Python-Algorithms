e18f381aeeec8f5885ccf5f37d64bae955f9ec5b
stalk-calvin <stalk.calvin@gmail.com>
2016-12-19 23:39:39 -0800

    CL: add reverse string python

--- calvin/strings.py
+++ calvin/strings.py
@@ -2,8 +2,20 @@ class Strings(object):
     """
     Implementation on String Manipulation
     """
-def find_the_difference(s, t):
-    ans = 0
-    for c in s + t:
-        ans ^= ord(c)
-    return chr(ans)
+    def find_the_difference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        ans = 0
+        for c in s + t:
+            ans ^= ord(c)
+        return chr(ans)
+
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
--- tests/strings.py
+++ tests/strings.py
@@ -2,14 +2,26 @@ from __future__ import absolute_import
 
 import unittest
 
-from calvin import strings
+from calvin.strings import Strings
 
-class TestStrings(unittest.TestCase):
+class StringsTest(unittest.TestCase):
     """
     Test Strings method
     """
+    def setUp(self):
+        self.fixture = Strings()
+
     def test_find_the_difference(self):
         s = "abcd"
         t = "abcde"
-        actual = strings.find_the_difference(s, t)
-        self.assertEqual("e", actual)
+        actual = self.fixture.find_the_difference(s, t)
+        self.assertEquals("e", actual)
+
+    def test_reverse_string(self):
+        input = "abcd"
+        actual = self.fixture.reverseString(input)
+        expected = "dcba"
+        self.assertEquals(expected, actual)
+
+    def tearDown(self):
+        del self.fixture
