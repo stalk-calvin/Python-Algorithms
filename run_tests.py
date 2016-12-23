fc1c4d471b97ecee3664b5ac907c0ede7b84d85c
stalk-calvin <stalk.calvin@gmail.com>
2016-12-22 18:02:12 -0800

    CL: Changes for coverage report

--- run_tests.py
+++ run_tests.py
@@ -8,7 +8,7 @@ tests_dir = 'tests'
 for file in os.listdir(tests_dir):
     #logging.basicConfig(level=logging.DEBUG)
 
-    if (file.startswith('__') and file.endswith('.py')) or file.endswith('.pyc'):
+    if (file.startswith('.') or not file.endswith('.py') or file.startswith('__') and file.endswith('.py')) or file.endswith('.pyc'):
         continue
     qual_file = os.path.join(tests_dir, file)
     module = qual_file.replace('/', '.')[:-3]  # leave off .py
